7ece83e8e6eda38f25311106cc869deeb3d4b180
Ann Marusina <an.marusina2010@yandex.ru>
2023-09-20 23:31:45 +0300

    chngs

---
+++ src/js/health.js
@@ -0,0 +1,16 @@
+export default function showHealth(obj) {
+    let result;
+    if (obj.health > 50) {
+      result = 'healthy';
+    }
+    if (obj.health <= 50 && obj.health > 15) {
+      result = 'wounded';
+    }
+    if (obj.health <= 15 && obj.health > 0) {
+      result = 'critical';
+    }
+    if (Number.isNaN(obj.health) || obj.health === undefined || obj.health < 0 || typeof (obj.health) !== 'number') {
+      throw new Error('Parameter is not a number!');
+    }
+    return result;
+  }
